02c7630a9a74d4dd7be5d73456df2e6ae8b1b2d2
Grace-Girl <827657511@qq.com>
2018-07-28 08:35:57 +0800

    首页表单渲染

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # guestbook-express
+
+#### 1.页面跳转
+
+0.创建目录结构，并在views中添加index.html,publish.html静态页面
+
+1.安装bootstrap样式
+
+2.在app.js中，提供两个路由，以渲染index.html,publish.html，同时把node_modules开放出来
+
+3.设置标识，在点击发表留言的时候，跳转至发表留言href="/publish",跳转至首页href="/"
+
+#### 2.首页留言列表渲染
+
+1.创建db.json文件，自己编写相应的json数据，{posts:[ {},{},{} ] }; 将数据手动添加到 app.js中的render首页后边
+
+2.将index.html中，使用模板渲染json数据
+
+3.使用db.json文件中的数据，替换render首页后的数据；引入fs，读取db.json文件，在内部使用try，catch异常捕获，在try内转换json数据格式并渲染数据；
+
--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express')
 
+var fs=require('fs')
+
 var app = express()
 
 app.use('/node_modules/', express.static('./node_modules/'))
@@ -7,16 +9,61 @@ app.use('/node_modules/', express.static('./node_modules/'))
 // 在 Express 中配置使用 art-template 模板引擎
 app.engine('html', require('express-art-template'))
 
-app.get('/', function (req, res) {
-  res.render('index.html')
+app.get('/', function(req, res) {
+    // res.render('index.html', {
+    //     "posts": [
+
+    //         {
+    //             "id": 1,
+    //             "name": "张三",
+    //             "content": "hehehehe",
+    //             "time": "2018-01-17 12:01:01",
+    //         }, {
+    //             "id": 2,
+    //             "name": "张三2",
+    //             "content": "hehehehe",
+    //             "time": "2018-01-17 12:01:01",
+    //         }, {
+    //             "id": 3,
+    //             "name": "张三3",
+    //             "content": "hehehehe",
+    //             "time": "2018-01-17 12:01:01",
+    //         }, {
+    //             "id": 4,
+    //             "name": "张三4",
+    //             "content": "hehehehe",
+    //             "time": "2018-01-17 12:01:01",
+    //         }
+
+    //     ]
+    // })
+    // 
+    fs.readFile('./db.json',function(err,data){
+    	if(err){
+    		return res.render('500.html',{
+    			errMessage:err.message
+    		})
+    	}
+    	try{
+    		data=JSON.parse(data.toString())
+    		res.render('index.html',{
+    			posts:data.posts
+    		})
+    	}catch(err){
+    		return res.render('500.html',{
+    			errMessage:err.message
+    		})
+    	}
+    })
+    
 })
 
-app.get('/publish', function (req, res) {
-  res.render('publish.html')
+app.get('/publish', function(req, res) {
+    res.render('publish.html')
 })
-app.post('/publish',function(req,res){
-	res.render('')
+app.post('/publish', function(req, res) {
+    res.render('')
 })
-app.listen(3000, function () {
-  console.log('running...')
+app.listen(3000, function() {
+    console.log('running...')
 })
